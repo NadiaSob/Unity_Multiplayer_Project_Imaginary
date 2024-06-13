2441afd0773c82bd31dac68037e22c038f52fa04
NadiaSob <sobo_na@mail.ru>
2024-06-13 15:08:41 +0300

    Added UTP Transport

--- Imaginary/Assets/Scripts/Connection.cs
+++ Imaginary/Assets/Scripts/Connection.cs
@@ -1,7 +1,10 @@
-using UnityEngine;
+using System;
 using Mirror;
 using TMPro;
+using UnityEngine;
+using Utp;
 
+[RequireComponent(typeof(RelayManager))]
 public class Connection : MonoBehaviour
 {
     [SerializeField] private NetworkManager networkManager = null;
@@ -9,22 +12,61 @@ public class Connection : MonoBehaviour
     [Header("UI")]
     [SerializeField] private GameObject landingPagePanel = null;
     [SerializeField] private TMP_InputField ipAddress_InputField = null;
+    //[SerializeField] private TextMeshProUGUI joinCodeText = null;
+
+    private RelayManager _relayManager;
 
     private void Start()
     {
         //Screen.SetResolution(800, 600, false);
+        _relayManager = networkManager.GetComponent<RelayManager>();
     }
 
     public void HostLobby()
     {
-        networkManager.StartHost();
+        try
+        {
+            StartRelayHost(4);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
 
-        landingPagePanel.SetActive(false);
+        // networkManager.StartHost();
+        // landingPagePanel.SetActive(false);
+    }
+
+    public void StartRelayHost(int maxPlayers, string regionId = null)
+    {
+        _relayManager.AllocateRelayServer(maxPlayers, regionId,
+            joinCode =>
+            {
+                Debug.LogError($"Join by code: {joinCode}");
+                _relayManager.JoinCode = joinCode;
+                //joinCodeText.text = joinCode;
+                networkManager.StartHost();
+            },
+            () =>
+            {
+                UtpLog.Error($"Failed to start a Relay host.");
+            });
     }
 
     public void JoinClient()
     {
-        networkManager.networkAddress = ipAddress_InputField.text;
-        networkManager.StartClient();
+        string joinCode = ipAddress_InputField.text;
+        _relayManager.GetAllocationFromJoinCode(joinCode,
+            () =>
+            {
+                networkManager.StartClient();
+            },
+            () =>
+            {
+                UtpLog.Error($"Failed to join Relay server.");
+            });
+
+        // networkManager.networkAddress = ipAddress_InputField.text;
+        // networkManager.StartClient();
     }
 }
---
+++ Imaginary/Assets/UTPTransport/RelayNetworkManager.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+using Mirror;
+
+using UnityEngine;
+using Unity.Services.Relay.Models;
+
+namespace Utp
+{
+	public class RelayNetworkManager : NetworkManager
+	{
+		
+	}
+}
